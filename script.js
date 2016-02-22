d0e68136eec466b1cfc42f4b22e51624f82f09db
Elaine Feng <eleanor52@live.ca>
2016-02-22 01:53:53 -0800

    Testing form take 2

--- script.js
+++ script.js
@@ -11,32 +11,32 @@ $(document).ready(function(){
   
   
   
-  $("#contactForm").submit(function(event){
-    // cancels the form submission
-    event.preventDefault();
-    submitForm();
-  });
-  
-  function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
- 
-    $.ajax({
-        type: "POST",
-        url: "index.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            }
-        }
-    });
-  }
-  
-  function formSuccess(){
-    $( "#msgSubmit" ).removeClass( "hidden" );
-  }
+//  $("#contactForm").submit(function(event){
+//    // cancels the form submission
+//    event.preventDefault();
+//    submitForm();
+//  });
+//  
+//  function submitForm(){
+//    // Initiate Variables With Form Content
+//    var name = $("#name").val();
+//    var email = $("#email").val();
+//    var message = $("#message").val();
+// 
+//    $.ajax({
+//        type: "POST",
+//        url: "index.php",
+//        data: "name=" + name + "&email=" + email + "&message=" + message,
+//        success : function(text){
+//            if (text == "success"){
+//                formSuccess();
+//            }
+//        }
+//    });
+//  }
+//  
+//  function formSuccess(){
+//    $( "#msgSubmit" ).removeClass( "hidden" );
+//  }
   
 });	
