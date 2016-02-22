82c58f91740b8ce3d85243bf263b4097b7470c1c
Elaine Feng <eleanor52@live.ca>
2016-02-22 04:09:20 -0800

    Added basic skillbar section

--- script.js
+++ script.js
@@ -9,42 +9,13 @@ $(document).ready(function(){
 		}
   );
   
+  jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+  
   var contactform =  document.getElementById('contactForm');
     contactform.setAttribute('action', '//formspree.io/' + 'eleanor52' + '@' + 'live' + '.' + 'ca');
-  
-  $.ajax({
-    url: "//formspree.io/eleanor52@live.ca", 
-    method: "POST",
-    data: {message: "hello!"},
-    dataType: "json"
-  });
-  
-//  $("#contactForm").submit(function(event){
-//    // cancels the form submission
-//    event.preventDefault();
-//    submitForm();
-//  });
-//  
-//  function submitForm(){
-//    // Initiate Variables With Form Content
-//    var name = $("#name").val();
-//    var email = $("#email").val();
-//    var message = $("#message").val();
-// 
-//    $.ajax({
-//        type: "POST",
-//        url: "index.php",
-//        data: "name=" + name + "&email=" + email + "&message=" + message,
-//        success : function(text){
-//            if (text == "success"){
-//                formSuccess();
-//            }
-//        }
-//    });
-//  }
-//  
-//  function formSuccess(){
-//    $( "#msgSubmit" ).removeClass( "hidden" );
-//  }
   
 });	
